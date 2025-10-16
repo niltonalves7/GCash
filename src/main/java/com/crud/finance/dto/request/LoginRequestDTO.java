31f399fdbab64016fc28755991d17d411c320311
Nilton <nilttonalvez@gmail.com>
2025-10-16 17:45:08 -0300

    creating login dtos

--- src/main/java/com/crud/finance/dto/request/LoginRequestDTO.java
+++ src/main/java/com/crud/finance/dto/request/LoginRequestDTO.java
@@ -1,4 +1,13 @@
 package com.crud.finance.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequestDTO {
+    private String email;
+    private String password;
 }
--- src/main/java/com/crud/finance/dto/response/LoginResponseDTO.java
+++ src/main/java/com/crud/finance/dto/response/LoginResponseDTO.java
@@ -1,4 +1,15 @@
 package com.crud.finance.dto.response;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class LoginResponseDTO {
+
+    private String token;
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
 }
