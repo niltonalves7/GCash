48671fe3c18d2abf96cc2e27f675c58cc800cb52
Nilton <nilttonalvez@gmail.com>
2025-10-16 17:45:45 -0300

    creating authentication service

--- src/main/java/com/crud/finance/service/AuthenticationService.java
+++ src/main/java/com/crud/finance/service/AuthenticationService.java
@@ -1,4 +1,11 @@
 package com.crud.finance.service;
 
+import com.crud.finance.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 public class AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
 }
