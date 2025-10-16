f9b7f58fbb0687d8be887963481f9a6357c98638
Nilton <nilttonalvez@gmail.com>
2025-10-16 17:44:19 -0300

    creating authetication controller

--- src/main/java/com/crud/finance/controller/AuthenticationController.java
+++ src/main/java/com/crud/finance/controller/AuthenticationController.java
@@ -1,15 +1,12 @@
 package com.crud.finance.controller;
 
-import org.apache.coyote.Response;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.crud.finance.model.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
 @CrossOrigin(origins = "*")
-public class LoginController {
+public class AuthenticationController {
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponseDTO> login(@RequestBody LoginDTO dto) {
