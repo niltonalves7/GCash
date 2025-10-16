66b5de9dc6df77fe4f43b45495e4305152b19e61
Nilton <nilttonalvez@gmail.com>
2025-10-16 16:48:31 -0300

    creating user controller

--- src/main/java/com/crud/finance/controller/TransactionController.java
+++ src/main/java/com/crud/finance/controller/TransactionController.java
@@ -9,10 +9,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 
 @RestController
-@RequestMapping("/transfer")
+@RequestMapping("/transaction")
 @CrossOrigin(origins = "*")
 public class TransactionController {
 
@@ -23,28 +24,28 @@ public class TransactionController {
     }
 
     @GetMapping()
-    public ResponseEntity<List<Transaction>> getAllTransfers(){
-        return ResponseEntity.ok(transactionService.getAllTransfers());
+    public ResponseEntity<List<Transaction>> getAllTransactions(){
+        return ResponseEntity.ok(transactionService.getAllTransactions());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<TransactionResponseDTO> getTransferById(@PathVariable Long id){
-        return ResponseEntity.ok(transactionService.getTransferById(id));
+    public ResponseEntity<TransactionResponseDTO> getTransferById(@PathVariable UUID id){
+        return ResponseEntity.ok(transactionService.getTransactionById(id));
     }
 
     @PostMapping()
     public ResponseEntity<TransactionResponseDTO> createTransfer(@RequestBody TransactionRequestDTO dto ){
-        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.createTransfer(dto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(transactionService.createTransaction(dto));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<TransactionResponseDTO> updateTransfer(@PathVariable Long id, @RequestBody TransactionRequestDTO dto){
-        return ResponseEntity.ok(transactionService.updateTransfer(id, dto));
+    public ResponseEntity<TransactionResponseDTO> updateTransfer(@PathVariable UUID id, @RequestBody TransactionRequestDTO dto){
+        return ResponseEntity.ok(transactionService.updateTransaction(id, dto));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteTransfer(@PathVariable Long id){
-        transactionService.deleteTransfer(id);
+    public ResponseEntity<Void> deleteTransfer(@PathVariable UUID id){
+        transactionService.deleteTransaction(id);
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/crud/finance/controller/UserController.java
+++ src/main/java/com/crud/finance/controller/UserController.java
@@ -5,13 +5,12 @@ import com.crud.finance.dto.request.UserRequestDTO;
 import com.crud.finance.dto.response.UserResponseDTO;
 import com.crud.finance.model.User;
 import com.crud.finance.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/user")
@@ -27,5 +26,26 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(User dto) {
         return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable UUID id){
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> registerUser(@RequestBody UserRequestDTO dto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.registerUser(dto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> updateUser(@PathVariable UUID id, @RequestBody UserRequestDTO dto) {
+        return ResponseEntity.ok(userService.updateUser(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable UUID id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 }
