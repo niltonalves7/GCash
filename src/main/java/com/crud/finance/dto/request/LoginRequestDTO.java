925232876012682ec3b4cbefca3f39e93f9036cc
Nilton <nilttonalvez@gmail.com>
2025-10-17 17:27:01 -0300

    creating login dto

--- src/main/java/com/crud/finance/dto/request/LoginRequestDTO.java
+++ src/main/java/com/crud/finance/dto/request/LoginRequestDTO.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserLoginRequestDTO {
+public class LoginRequestDTO {
     private String email;
     private String password;
 }
