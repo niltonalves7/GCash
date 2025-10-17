4c2fdbe6c9d61c0763b405b7c50bd7a7532e1a64
Nilton <nilttonalvez@gmail.com>
2025-10-17 17:30:22 -0300

    start authentication filter

--- src/main/java/com/crud/finance/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/crud/finance/security/jwt/JwtAuthenticationFilter.java
@@ -1,4 +1,26 @@
 package com.crud.finance.security.jwt;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter {
+
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal (
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
+
 }
