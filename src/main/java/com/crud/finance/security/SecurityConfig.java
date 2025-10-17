dca43fe8896f50ce6894dcfed3bf6249f1ed5bd0
Nilton <nilttonalvez@gmail.com>
2025-10-17 17:29:53 -0300

    creating security configuration

--- src/main/java/com/crud/finance/security/SecurityConfig.java
+++ src/main/java/com/crud/finance/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.crud.finance.security.jwt;
+package com.crud.finance.security
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
