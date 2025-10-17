2516e98eea36f128bb221377c640cb9f84321432
Nilton <nilttonalvez@gmail.com>
2025-10-17 17:28:33 -0300

    creating response register

--- src/main/java/com/crud/finance/dto/response/RegisterResponseDTO.java
+++ src/main/java/com/crud/finance/dto/response/RegisterResponseDTO.java
@@ -9,7 +9,7 @@ import java.math.BigDecimal;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserResponseDTO {
+public class RegisterResponseDTO {
     private String name;
     private String email;
     private String accountNumber;
