1842d0aebfecb14fb09549d61fe501d791eace80
Nilton <nilttonalvez@gmail.com>
2025-10-20 17:45:32 -0300

    creating authentication filter

--- src/main/java/com/crud/finance/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/crud/finance/security/jwt/JwtAuthenticationFilter.java
@@ -1,26 +1,64 @@
 package com.crud.finance.security.jwt;
 
 
+import com.crud.finance.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
-public class JwtAuthenticationFilter {
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
-    private final UserDetailsService userDetailsService;
+    private final CustomUserDetailsService customUserDetailsService;
 
     @Override
     protected void doFilterInternal (
             HttpServletRequest request,
             HttpServletResponse response,
-            FilterChain filterChain
-    ) throws ServletException, IOException {
+            FilterChain filterChain)
 
+            throws ServletException, IOException {
+
+        final String authenticationHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if (authenticationHeader == null || !authenticationHeader.startsWith(("Bearer "))) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authenticationHeader.substring(7);
+        userEmail = jwtService.extractUsername(jwt);
+
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername((userEmail));
+
+            if (jwtService.isTokenValid(jwt, (com.crud.finance.model.User) userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails, null, userDetails.getAuthorities());
+
+                authenticationToken.setDetails( new WebAuthenticationDetailsSource()
+                        .buildDetails(request));
+
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
     }
 
 }
