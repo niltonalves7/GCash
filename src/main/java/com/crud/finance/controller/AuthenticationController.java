793a9ce0fdb3619b232e7249f6c60ada2f71dc95
Nilton <nilttonalvez@gmail.com>
2025-10-21 17:50:46 -0300

    refactor

--- src/main/java/com/crud/finance/controller/AuthenticationController.java
+++ src/main/java/com/crud/finance/controller/AuthenticationController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/authentication")
 @CrossOrigin(origins = "*")
 public class AuthenticationController {
 
