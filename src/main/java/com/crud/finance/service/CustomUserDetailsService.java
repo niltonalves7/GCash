f5f44806651f3c4cbd9f1548a9cd88680b6c1f15
Nilton <nilttonalvez@gmail.com>
2025-10-28 17:30:00 -0300

    refactor

--- src/main/java/com/crud/finance/service/CustomUserDetailsService.java
+++ src/main/java/com/crud/finance/service/CustomUserDetailsService.java
@@ -1,30 +1,24 @@
 package com.crud.finance.service;
 
-import com.crud.finance.entity.User;
 import com.crud.finance.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-
 @Service
-public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
+public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public UserDetailsService(UserRepository userRepository) {
+    public CustomUserDetailsService(UserRepository userRepository) {
+
         this.userRepository = userRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
+        return userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getEmail(),
-                user.getPassword(),
-                Collections.singletonList(user::getRole));
     }
 }
