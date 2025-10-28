81436586157469b149a71beafe36dd90299d4a30
Nilton <nilttonalvez@gmail.com>
2025-10-28 17:29:30 -0300

    refactor

--- src/main/java/com/crud/finance/service/AuthenticationService.java
+++ src/main/java/com/crud/finance/service/AuthenticationService.java
@@ -6,11 +6,12 @@ import com.crud.finance.dto.response.LoginResponseDTO;
 import com.crud.finance.dto.response.RegisterResponseDTO;
 import com.crud.finance.exceptions.ResourceNotFoundException;
 import com.crud.finance.mapper.UserMapper;
-import com.crud.finance.model.User;
+import com.crud.finance.entity.User;
 import com.crud.finance.repository.UserRepository;
 import com.crud.finance.security.jwt.JwtService;
 import com.crud.finance.validator.UserValidator;
-import com.crud.finance.exceptions.BadRequestException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.concurrent.ThreadLocalRandom;
@@ -21,36 +22,42 @@ public class AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService) {
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtService jwtService, AuthenticationManager authenticationManager) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
         this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
     }
 
-    public RegisterResponseDTO registerUser(RegisterRequestDTO dto) {
-        UserValidator.validate(dto);
+    public RegisterResponseDTO registerUser(RegisterRequestDTO requestDTO) {
+        UserValidator.validate(requestDTO);
 
-        if (userRepository.existsByEmail(dto.getEmail())){
+        if (userRepository.existsByEmail(requestDTO.getEmail())){
             throw new RuntimeException("Email already registered");
         }
 
         String accountNumber = generateUniqueAccountNumber();
-        User user = UserMapper.toEntity(dto);
+        User user = UserMapper.toEntity(requestDTO);
         user.setAccountNumber(accountNumber);
-        user.setPassword(passwordEncoder.encode(dto.getPassword()));
+        user.setPassword(passwordEncoder.encode(requestDTO.getPassword()));
         userRepository.save(user);
 
         return UserMapper.toDTO(user);
     }
 
-    public LoginResponseDTO login(LoginRequestDTO request) {
-        User user = userRepository.findByEmail(request.getEmail())
-                .orElseThrow(() -> new ResourceNotFoundException(("User not found!")));
+    public LoginResponseDTO login(LoginRequestDTO requestDTO) {
 
-        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
-            throw new BadRequestException("Invalid credentials");
-        }
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        requestDTO.getEmail(),
+                        requestDTO.getPassword()
+                )
+        );
+
+        User user = userRepository.findByEmail(requestDTO.getEmail())
+                .orElseThrow(() -> new ResourceNotFoundException(("User not found!")));
 
         String token = jwtService.generateToken(user);
         String refreshToken = jwtService.generateRefreshToken(user);
